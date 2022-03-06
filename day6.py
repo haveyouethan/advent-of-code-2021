c68692c71cfec7c789fe80b11a3550dd9c5ce255
Ethan <32772165+haveyouethan@users.noreply.github.com>
2022-03-07 00:23:57 +0800

    Update day6.py
    added part two solution, with new approach

--- day6.py
+++ day6.py
@@ -48,3 +48,48 @@ print('test cases passed')
 fish = Lanternfish(inputs)
 fish.days(80)
 print(fish.count_fishes())
+
+
+# PART TWO #
+
+class Fishstock:
+    """count by census buckets of fish age, 
+    instead of by individual fishes"""
+    def __init__(self, inputs):
+        self.day = -1
+        
+        tracker = [0]*9
+        for fish in inputs:
+            tracker[fish] += 1
+        self.fishes = tracker
+
+    def count_fishes(self):
+        return sum(self.fishes)
+    
+    def next_day(self):
+        self.day += 1
+        self.fishes = self.next_fishes()
+    
+    def days(self,num_days):
+        for day in range(num_days):
+            self.next_day()
+        
+    def next_fishes(self):
+        new = self.fishes[1:] + self.fishes[:1]
+        new[6] += self.fishes[0]
+        return new
+        
+tc_fish = Fishstock(testcase)
+tc_fish.days(18)
+assert tc_fish.count_fishes()==26 
+tc_fish.days(80-18)
+assert tc_fish.count_fishes()==5934 
+print('test cases (part 2) passed')
+
+
+fish = Fishstock(inputs)
+fish.days(80)
+print(fish.count_fishes())
+
+fish.days(256-80)
+print(fish.count_fishes())
