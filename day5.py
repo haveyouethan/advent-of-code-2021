01b864840bcb7a4ef326a9260f4ca00ccd0be171
Ethan <32772165+haveyouethan@users.noreply.github.com>
2022-03-06 23:11:47 +0800

    Update day5.py

--- day5.py
+++ day5.py
@@ -48,3 +48,53 @@ for line in trunc_inputs:
 print(score(diag1))
 
 # PART 2 #
+
+def update_diag2(line,diag):
+    
+    x1,y1,x2,y2 = line
+    new = diag
+
+    row_max = max(x1,x2)
+    row_min = min(x1,x2)
+    col_max = max(y1,y2)
+    col_min = min(y1,y2)
+    
+    
+    if x1 == x2:
+        row = x1
+        for col in range(col_min,col_max+1):
+            new[row][col] += 1
+            
+    elif y1 == y2:
+        col = line[1]
+        for row in range(row_min,row_max+1):
+            new[row][col] += 1
+                
+    elif abs(x1-x2) == abs(y1-y2):
+        dx = 1 if x2 > x1 else -1
+        dy = 1 if y2 > y1 else -1
+
+        row = x1
+        col = y1
+        while row!=x2 + 1*dx:
+            new[row][col] += 1
+            row += 1*dx
+            col += 1*dy
+
+        
+    else:
+        print("error: non-linear, non-diagonal vents")
+        
+    return new
+
+
+
+trunc_inputs_pt2 = [x for x in inputs if x[0]==x[2] or x[1]==x[3] 
+                    or abs(x[0]-x[2])==abs(x[1]-x[3])]
+
+diag2 = init_diag()
+for line in trunc_inputs_pt2:
+    diag2 = update_diag2(line,diag2)
+
+print(score(diag2))
+
